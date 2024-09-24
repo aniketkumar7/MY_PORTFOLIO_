092a90f9d15ad93a7b0c4ed5495af0b298402741
aniketkumar7 <aniketkumar639597@gmail.com>
2024-09-24 19:38:21 +0530

    Update the styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,15 @@
-import { PropTypes } from 'prop-types';
+import PropTypes from "prop-types";
 import { gsap } from "gsap";
-import {motion} from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import "./menu.css";
 
-const Menu = ({sectionRefs,textColor}) => {
+const Menu = ({ sectionRefs, textColor }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(null);
   const menuItemsRef = useRef([]);
+  const logoRef = useRef(null);
+  const buttonRef = useRef(null);
+  const menuRef = useRef(null);
 
   // Function to handle item clicks
   const handleItemClick = (index) => {
@@ -25,66 +27,83 @@ const Menu = ({sectionRefs,textColor}) => {
   // Effect to handle menu animations based on menuOpen state
   useEffect(() => {
     if (menuOpen) {
-      gsap.to(".fullscreen-menu", {
+      gsap.to(menuRef.current, {
         x: 0,
-        duration: 0.5,
+        duration: 0.8,
         width: "100%",
         opacity: 1,
         visibility: "visible",
-        ease: "power1.inOut",
+        ease: "power3.inOut",
       });
       gsap.fromTo(
         menuItemsRef.current,
-        { y: 80, opacity: 0, display: "block" },
+        { y: 80, opacity: 0, scale: 0.8, rotation: 15, display: "block" },
         {
           y: 0,
           opacity: 1,
-          duration: 0.6,
-          stagger: 0.3,
-          ease: "power1.inOut",
+          scale: 1,
+          rotation: 0,
+          duration: 0.8,
+          stagger: 0.2,
+          ease: "power3.inOut",
         }
       );
     } else {
       gsap.to(menuItemsRef.current, {
         opacity: 0,
+        scale: 0.8,
+        rotation: -15,
         duration: 0.5,
-        stagger: 0.2,
-        ease: "power1.inOut",
+        stagger: 0.1,
+        ease: "power3.inOut",
         onComplete: () => {
           gsap.set(menuItemsRef.current, { display: "none" });
         },
       });
-      gsap.to(".fullscreen-menu", {
+      gsap.to(menuRef.current, {
         duration: 0.6,
         width: "0%",
         opacity: 0,
         visibility: "hidden",
-        delay: 1.2,
-        ease: "power1.inOut",
+        ease: "power3.inOut",
+        delay: 1.3,
       });
     }
   }, [menuOpen]);
+
+  // Effect to animate navbar elements
+  useEffect(() => {
+    gsap.fromTo(
+      logoRef.current,
+      { opacity: 0, y: -20 },
+      { opacity: 1, y: 0, duration: 1, delay:5, ease: "power2.out" }
+    );
+    gsap.fromTo(
+      buttonRef.current,
+      { opacity: 0, y: -20 },
+      { opacity: 1, y: 0, duration: 1, delay:5.2, ease: "power2.out"}
+    );
+  }, []);
 
   return (
     <div className="nav">
       <nav className="navbar">
         {/* Logo */}
-        <div className="logo" style={{ color: textColor }}>
+        <div className="logo" ref={logoRef} style={{ color: textColor }}>
           <a href="#">ANIKET</a>
         </div>
 
         {/* Button to toggle the menu */}
         <button
           className="menu-button"
+          ref={buttonRef}
           style={{ backgroundColor: textColor }}
           onClick={toggleMenu}>
           {menuOpen ? "Close" : "Menu"}
         </button>
 
         {/* Fullscreen menu that animates in and out */}
-        <motion.div
-          className="fullscreen-menu menu"
-          animate={{ x: menuOpen ? 0 : 100, opacity: menuOpen ? 1 : 0 }}>
+        <div className="fullscreen-menu menu" ref={menuRef}>
           <ul className="menu-items">
             {["Home", "Skills", "About", "Projects", "Contact"].map(
               (item, index) => (
@@ -101,21 +120,19 @@ const Menu = ({sectionRefs,textColor}) => {
                   </a>
                 </li>
               )
-
             )}
           </ul>
-        </motion.div>
+        </div>
       </nav>
     </div>
   );
 };
 
-
 // Props Validation
 Menu.propTypes = {
   sectionRefs: PropTypes.array.isRequired,
   textColor: PropTypes.string.isRequired,
-}
+};
 
 // Export the Menu component
 export default Menu;
